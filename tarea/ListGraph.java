63df63b5065dda64a1725d86e845187c04c3e964
Paul Alexander <pluquec@unsa.edu.pe>
2021-08-04 21:06:33 -0500

    implemenatado para obtener los adyacentes de un nodo

--- tarea/ListGraph.java
+++ tarea/ListGraph.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ListGraph {
     private static final int MAX = 100000; // numero maximo de vertices
     private int numVertes; // el numero de verices actuales
@@ -14,7 +16,15 @@ public class ListGraph {
         arrVertes = new Vertex[maxVertes];
     }
 
-    
+    // regesa la lista de adyacentes de nodo en la posicion v
+    public ArrayList<Arc> getLinks(int v) throws Exception {
+        if (v < 0 || v >= numVertes) {
+            throw new Exception("Vértice fuera de rango");
+        } else if (arrVertes[v] == null) {
+            throw new Exception("Vértice no existe");
+        }
 
+        return arrVertes[v].getLinks();
+    }
 
 }
