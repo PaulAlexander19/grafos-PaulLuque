21e026427b8f23bec9ec7acd41aa28ea4e3d2009
Paul Alexander <pluquec@unsa.edu.pe>
2021-08-04 20:46:14 -0500

    creando la clase Arc

--- tarea/Arc.java
+++ tarea/Arc.java
@@ -1,3 +1,25 @@
+// sera para enlazar a otros vertices
 public class Arc {
-    
+    private int positionNext; // posicion del nodo siguiente
+    private double weight;
+
+    public Arc(int destino) {
+        positionNext = destino;
+    }
+
+    public Arc(int destino, double weight) {
+        positionNext = destino;
+        this.weight = weight;
+    }
+
+    public int getDestino() {
+        return positionNext;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Arc other = (Arc) obj;
+        return (this.positionNext == other.positionNext && this.weight == other.weight);
+    }
+
 }
