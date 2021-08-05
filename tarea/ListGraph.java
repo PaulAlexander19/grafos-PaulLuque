efdff3d487954b75a5f87d7961f4ef32d5347f81
Paul Alexander <pluquec@unsa.edu.pe>
2021-08-04 21:01:40 -0500

    creando la clase ListGraph, atributos constructores

--- tarea/ListGraph.java
+++ tarea/ListGraph.java
@@ -1,3 +1,20 @@
-public class ListGraph{
+public class ListGraph {
+    private static final int MAX = 100000; // numero maximo de vertices
+    private int numVertes; // el numero de verices actuales
+    private int maxVertes; // el nuemro maximo de vertices
+    Vertex[] arrVertes; // el arreglo de los vertices
+
+    public ListGraph() {
+        this(MAX);
+    }
+
+    public ListGraph(int maxVertes) {
+        this.maxVertes = maxVertes;
+        this.numVertes = 0;
+        arrVertes = new Vertex[maxVertes];
+    }
+
     
+
+
 }
