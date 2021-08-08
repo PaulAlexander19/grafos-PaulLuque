f79a0a388d4f67263fea3470e640019a19a310bf
Paul Alexander <pluquec@unsa.edu.pe>
2021-08-08 01:15:17 -0500

    convirtiendo la clase Vertex en generico

--- tarea/Vertex.java
+++ tarea/Vertex.java
@@ -2,60 +2,40 @@ import java.util.ArrayList;
 
 // clase de los vertices 
 
-public class Vertex {
-    String name; // Sera lo que lo representa, puede ser cualquier cosa
-    int numVertex; // para la posicion en el arreglo
-    ArrayList<Arc> links; // enlaces del vertice
+public class Vertex<T> {
+    T data; // Sera lo que lo representa, puede ser cualquier cosa
+    ArrayList<Arc<T>> links; // enlaces del vertice
 
-    public Vertex(String name) {
-        this.name = name;
-        links = new ArrayList<Arc>();
-    }
-
-    public Vertex(String name, int numVertex) {
-        this.name = name;
-        this.numVertex = numVertex;
-        links = new ArrayList<Arc>();
-    }
-
-    // Indica el número que identificará este vértice
-    public void asigPosition(int n) {
-        numVertex = n;
+    public Vertex(T data) {
+        this.data = data;
+        links = new ArrayList<Arc<T>>();
     }
 
     @Override
     public boolean equals(Object obj) {
-        Vertex other = (Vertex) obj;
-        return (this.name.equals(other.name));
+        Vertex<T> other = (Vertex<T>) obj;
+        return (this.data.equals(other.data) && (links.equals(other.links)));
     }
 
-    public String getName() {
-        return this.name;
+    public T getData() {
+        return this.data;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setData(T data) {
+        this.data = data;
     }
 
-    public int getNumVertex() {
-        return this.numVertex;
-    }
-
-    public void setNumVertex(int numVertex) {
-        this.numVertex = numVertex;
-    }
-
-    public ArrayList<Arc> getLinks() {
+    public ArrayList<Arc<T>> getLinks() {
         return this.links;
     }
 
-    public void setLinks(ArrayList<Arc> links) {
+    public void setLinks(ArrayList<Arc<T>> links) {
         this.links = links;
     }
 
     @Override
     public String toString() {
-        return "Arc: (" + this.name + ")";
+        return "(" + this.data + ")";
     }
 
 }
